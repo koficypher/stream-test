62f8e11cfb5cb63d7d80c6a54f8eefa72f241f63
Sitsofe K. Fefeti <skcypher6@gmail.com>
2019-07-18 14:59:28 +0000

    base scaffolding

---
+++ public/index.js
@@ -0,0 +1,12 @@
+(function(){
+   
+    function init() {
+        const VP = document.getElementById('videoPlayer')
+        const VPToggle = document.getElementById('toggleButton')
+
+        VPToggle.addEventListener('click', function() {
+          if (VP.paused) VP.play()
+          else VP.pause()
+        })
+    }
+})();
